b14fb7d5c723b29f012f80f045671f12ad5ebe4e
taeshik Hwang <ghjkghj@naver.com>
2017-07-07 10:59:58 +0900

    Creating a User Scheme

---
+++ basicMysqlSite/models/user.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: models => {
+        // associations can be defined here
+      }
+    }
+  });
+  return User;
+};
