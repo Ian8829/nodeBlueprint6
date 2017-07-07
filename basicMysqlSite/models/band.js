1643c913c57276e58ebe205d72cf0051be7bf123
taeshik Hwang <ghjkghj@naver.com>
2017-07-07 11:02:35 +0900

    Creating Band Schema

---
+++ basicMysqlSite/models/band.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Band = sequelize.define('Band', {
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    album: DataTypes.STRING,
+    year: DataTypes.STRING,
+    UserId: DataTypes.INTEGER
+  }, {
+    classMethods: {
+      associate: models => {
+        // associations can be defined here
+      }
+    }
+  });
+  return Band;
+};
